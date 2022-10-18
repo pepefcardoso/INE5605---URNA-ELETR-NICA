95dbbcd4a3adeedf0cf41bc69a2c8b64f15d1a7f
Daniel <danielsottovia@gmail.com>
2022-10-18 01:40:03 -0300

    1:39
    Mais alguns avanços na classe tela_chapa e controlador_chapa

--- limite/abstract_tela.py
+++ limite/abstract_tela.py
@@ -5,3 +5,9 @@ class Tela(ABC):
     @abstractmethod
     def pega_opcao(self):
         pass
+
+    # Vamos padrozinar essa função, não precisa colocar cada um com um nome personalizado;
+    '''
+    @abstractmethod
+    def mostra_tela(self):
+        pass'''
--- limite/tela_chapa.py
+++ limite/tela_chapa.py
@@ -1,5 +1,22 @@
+from abstract_tela import Tela
 
+class TelaChapa(Tela):
 
-class TelaChapa():
-    def __init__(self):
+    def mostra_tela(self):
+        print('CHAPA')
+        print('1 - Lista de Chapas.')
+        print('2 - Adicionar Chapa.')
+        print('3 - Remove Chapa.')
+
+    def checa_numero(self, mensagem: str = '', inteiros_validos: [] = None):
+        while True:
+            valor_lido = int(input(mensagem))
+            try:
+                if valor_lido not in inteiros_validos:
+                    raise ValueError
+                return valor_lido
+            except ValueError:
+            #parei aqui muito sono
+
+    def pega_opcao(self):
         pass
