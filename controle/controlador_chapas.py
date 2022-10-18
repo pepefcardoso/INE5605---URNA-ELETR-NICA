5d13319c3acae1c61b4bec7cb3e6f8aeb33c0158
Pepefcardoso <pppfcardoso@gmail.com>
2022-10-18 17:10:12 -0300

    Alterações na classe Chapa

--- controle/controlador_chapas.py
+++ controle/controlador_chapas.py
@@ -8,17 +8,29 @@ class ControladorChapas():
         self.__chapas = []
 
     def lista_chapas(self):
-        pass
+        for chapa in self.__chapas:
+            nome = chapa.nome
+            self.__tela_chapa.mostra_chapa(nome)
+        if self.__chapas == []:
+            self.__tela_chapa.mostra_mensagem("\nNão existem chapas cadastradas!\n")
 
     def adiciona_chapa(self):
-        pass
+        nova_chapa = Chapa(self.__tela_chapa.pega_nome_chapa())
+        for chapa in self.__chapas:
+            if chapa.nome == nova_chapa.nome:
+                self.__tela_chapa.mostra_mensagem("\nJá existe uma chapa cadastrada com este nome!\n")
+                return None
+        self.__chapas.append(nova_chapa)
 
     def remove_chapa(self):
         pass
 
+    def altera_chapa(self):
+        pass
+
     def mostra_tela_opcoes(self):
         opcoes = {1: self.lista_chapas, 2: self.adiciona_chapa,
-                3:self.remove_chapa}
+                3:self.remove_chapa, 4:self.altera_chapa}
         while True:
             opcao = self.__tela_chapa.mostra_tela_chapas()
             if opcao == 0:
--- limite/tela_chapas.py
+++ limite/tela_chapas.py
@@ -3,12 +3,13 @@ from limite.abstract_tela import Tela
 class TelaChapa(Tela):
 
     def mostra_tela_chapas(self):
-        print('CHAPA')
+        print('----- CHAPA -----')
         print('1 - Lista de Chapas')
         print('2 - Adicionar Chapa')
-        print('3 - Remove Chapa')
+        print('3 - Remover Chapa')
+        print('4 - Remover Chapa')
         print('0 - Retornar ao Menu Principal')
-        opcao = self.pega_opcao('Escolha uma opção: ', [1, 2, 3, 0])
+        opcao = self.pega_opcao('Escolha uma opção: ', [1, 2, 3, 4, 0])
         return opcao
 
     def pega_opcao(self, mensagem: str = "", opcoes_validas = None):
@@ -23,3 +24,20 @@ class TelaChapa(Tela):
                 print("Opção indisponível, tente uma opção válida.")
                 if opcoes_validas:
                     print('Opções válidas: ', opcoes_validas)
+
+    def mostra_chapa(self, nome: str):
+        print(f'Chapa: {nome}')
+
+    def mostra_mensagem(self, mensagem: str):
+        print(mensagem)
+
+    def pega_nome_chapa(self):
+        while True:
+            leitura = input("Insira o nome da Chapa: ")
+            try:
+                nome = str(leitura)
+                if not isinstance(nome, str):
+                    raise ValueError
+                return nome
+            except ValueError:
+                print("Por favor, insira um nome válido")
