951017b27a71e0d8f81c7ed961369631c1be8da3
pepefcardoso <pppfcardoso@gmail.com>
2022-10-26 09:27:44 -0300

    Correções no ctrl de Chapas

--- controle/controlador_chapas.py
+++ controle/controlador_chapas.py
@@ -26,16 +26,16 @@ class ControladorChapas():
                 self.__tela_chapa.mostra_entidade(nome_chapa)
 
     def adiciona_chapa(self):
-        nome_chapa = self.__tela_eleitores.pega_nome_chapa()
+        nome_chapa = self.__tela_chapa.pega_nome_chapa()
         for chapa in self.__chapas:
             if chapa.nome == nome_chapa:
                 self.__tela_chapa.mostra_mensagem("\nJÁ EXISTE CHAPA CADASTRADA COM ESTE NOME!\n")
                 return None
         self.__tela_chapa.mostra_mensagem("\nCHAPA CADASTRADA COM SUCESSO!\n")
-        self.__eleitores.append(Chapa(nome_chapa))
+        self.__chapas.append(Chapa(nome_chapa))
 
     def remove_chapa(self):
-        nome_chapa = self.__tela_eleitores.pega_nome_chapa()
+        nome_chapa = self.__tela_chapa.pega_nome_chapa()
         for chapa in self.__chapas:
             if chapa.nome == nome_chapa:
                 self.__chapas.remove(chapa)
@@ -43,10 +43,10 @@ class ControladorChapas():
         self.__tela_chapa.mostra_mensagem("\nNÃO EXISTE CHAPA CADASTRADA COM ESTE NOME!\n")
 
     def altera_chapa(self):
-        nome_chapa = self.__tela_eleitores.pega_nome_chapa()
+        nome_chapa = self.__tela_chapa.pega_nome_chapa()
         for chapa in self.__chapas:
             if chapa.nome == nome_chapa:
-                nova_chapa = self.__tela_eleitores.pega_nome_chapa()
+                nova_chapa = self.__tela_chapa.pega_nome_chapa()
                 chapa.nome = nova_chapa
-                return self.__tela_eleitores.mostra_mensagem('\nCHAPA ALTERADA COM SUCESSO\n')
-        self.__tela_eleitores.mostra_mensagem('\nNÃO EXISTE CHAPA CADASTRADA COM ESTE NOME!\n')
+                return self.__tela_chapa.mostra_mensagem('\nCHAPA ALTERADA COM SUCESSO\n')
+        self.__tela_chapa.mostra_mensagem('\nNÃO EXISTE CHAPA CADASTRADA COM ESTE NOME!\n')
