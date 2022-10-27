40ce5aa75860dddc22658d2f40d42fee0602ab1c
pepefcardoso <pppfcardoso@gmail.com>
2022-10-27 19:40:04 -0300

    alterações na entidade Voto

--- entidade/voto.py
+++ entidade/voto.py
@@ -1,9 +1,13 @@
+from entidade.categoria import Categoria
+
+
 class Voto:
-    def __init__(self, voto_reitor: int, voto_grad: int, voto_pesquisa: int, voto_extensa: int):
+    def __init__(self, voto_reitor: int, voto_grad: int, voto_pesq: int, voto_ext: int, categoria: Categoria):
         self.__voto_reitor = voto_reitor
         self.__voto_grad = voto_grad
-        self.__voto_pesquisa = voto_pesquisa
-        self.__voto_extensao = voto_extensa
+        self.__voto_pesq = voto_pesq
+        self.__voto_ext = voto_ext
+        self.__categoria = categoria
 
     @property
     def voto_reitor(self):
@@ -14,9 +18,38 @@ class Voto:
         return self.__voto_grad
 
     @property
-    def voto_pesquisa(self):
-        return self.__voto_pesquisa
+    def voto_pesq(self):
+        return self.__voto_pesq
 
     @property
-    def voto_extensao(self):
-        return self.__voto_extensao
+    def voto_ext(self):
+        return self.__voto_ext
+
+    @property
+    def categoria(self):
+        return self.__categoria
+
+    @voto_reitor.setter
+    def voto_reitor(self, voto_reitor: int):
+        if isinstance(voto_reitor, int):
+            self.__voto_reitor = voto_reitor
+
+    @voto_grad.setter
+    def voto_grad(self, voto_grad: int):
+        if isinstance(voto_grad, int):
+            self.__voto_grad = voto_grad
+
+    @voto_pesq.setter
+    def voto_pesq(self, voto_pesq: int):
+        if isinstance(voto_pesq, int):
+            self.__voto_pesq = voto_pesq
+
+    @voto_ext.setter
+    def voto_ext(self, voto_ext: int):
+        if isinstance(voto_ext, int):
+            self.__voto_ext = voto_ext
+
+    @categoria.setter
+    def categoria(self, categoria: Categoria):
+        if isinstance(categoria, Categoria):
+            self.__categoria = categoria
