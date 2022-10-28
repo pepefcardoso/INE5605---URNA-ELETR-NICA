b4a983edec5c50559d88397580a51cd9a02deb22
Pepefcardoso <pppfcardoso@gmail.com>
2022-10-28 17:25:30 -0300

    Implementação da classe Registros

--- controle/controlador_registro.py
+++ controle/controlador_registro.py
@@ -1,6 +1,33 @@
+from limite.tela_registros import TelaRegistros
+from entidade.eleitor import Eleitor
+from entidade.candidato import Candidato
+from entidade.voto import Voto
+
+
 class ControladorRegistro:
     def __init__(self, controlador_urna):
         self.__controlador_urna = controlador_urna
+        self.__tela_registros = TelaRegistros()
 
     def mostra_tela_inicial(self):
+        opcoes = {1: self.abre_registros_geral, 
+                  2: self.abre_registros_1_turno,
+                  3: self.abre_registros_2_turno}
+        while True:
+            self.__tela_registros.abre_tela_inicial()
+            opcao = self.__tela_registros.pega_opcao()
+            if opcao == 0:
+                break
+            opcoes[opcao]()
+
+    def mostra_tela_registros(self):
+        pass
+
+    def abre_registros_1_turno(self):
+        pass
+
+    def abre_registros_2_turno(self):
+        pass
+
+    def abre_registros_geral(self):
         pass
--- limite/tela_registros.py
+++ limite/tela_registros.py
@@ -1,1 +1,21 @@
-pass
+from limite.abstract_tela import TelaAbstrata
+
+
+class TelaRegistros(TelaAbstrata):
+    def abre_tela_inicial(self):
+        super(TelaRegistros, self).abre_tela_inicial('REGISTROS',
+                                                      ['REGISTROS GERAIS', 'REGISTROS 1º TURNO', 'REGISTROS 2º TURNO'],
+                                                      'VOLTAR AO MENU INICIAL')
+
+    def abre_tela_registros(self):
+        pass
+
+    def pega_opcao(self):
+        opcao = super(TelaRegistros, self).pega_opcao('Escolha uma opção: ', [1, 2, 3, 0])
+        return opcao
+
+    def mostra_entidade(self, dados):
+        super(TelaRegistros, self).mostra_entidade(dados)
+
+    def mostra_mensagem(self, mensagem):
+        super(TelaRegistros, self).mostra_mensagem(mensagem)
