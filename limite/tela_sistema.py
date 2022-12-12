117c674fa77616fc72bd3416f1d07b8f519083bd
Daniel <danielsottovia@gmail.com>
2022-12-12 15:53:36 -0300

    Tela Sistema
    Centralizando e deixando todos os botões com o mesmo tamanho

--- limite/tela_sistema.py
+++ limite/tela_sistema.py
@@ -7,12 +7,13 @@ class TelaSistema():
 
     def tela_opcoes(self):
         psg.ChangeLookAndFeel('Reddit')
-        layout = [[psg.Button('ELEITORES')], [psg.Button('CANDIDATOS')],
-                  [psg.Button('CATEGORIAS')], [psg.Button('CARGOS')],
-                  [psg.Button('CHAPAS')], [psg.Button('CONFIGURAÇÕES')],
-                  [psg.Button('VOTAÇÃO')], [psg.Button('RELATÓRIOS')],
-                  [psg.Button('SAIR')]]
-        self.__window = psg.Window('URNA ELETRÔNICA UFSC', size=(1080,720)).Layout(layout)
+        layout = [[psg.Button('ELEITORES', size=(15, 1))], [psg.Button('CANDIDATOS', size=(15, 1))],
+                  [psg.Button('CATEGORIAS', size=(15, 1))], [psg.Button('CARGOS', size=(15, 1))],
+                  [psg.Button('CHAPAS', size=(15, 1))], [psg.Button('CONFIGURAÇÕES', size=(15, 1))],
+                  [psg.Button('VOTAÇÃO', size=(15, 1))], [psg.Button('RELATÓRIOS', size=(15, 1))],
+                  [psg.Button('SAIR', size=(15, 1))]]
+        layout = [[psg.Sizer(0, 350), psg.Column([[psg.Sizer(250, 0)]] + layout, element_justification='c', pad=(0, 0))]]
+        self.__window = psg.Window('URNA ELETRÔNICA UFSC', layout, margins=(0, 0))
 
     def abre(self):
         button, values = self.__window.Read()
