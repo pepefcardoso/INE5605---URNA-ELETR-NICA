e44f531b1cb3d7b8df6486a19e6246ff9545324c
Daniel <danielsottovia@gmail.com>
2022-12-12 14:37:12 -0300

    Alteração na tela da Chapa
    Foi centralizado e redimensionado a tela.

--- limite/tela_chapas.py
+++ limite/tela_chapas.py
@@ -13,14 +13,15 @@ class TelaChapas():
                              key='LISTA',
                              vertical_scroll_only=True,
                              auto_size_columns=False,
-                             col_widths=(57,57),
-                             num_rows=38,
+                             col_widths=(30,30),
+                             num_rows=15,
                              enable_events=True)],
                   [psg.Button('ADICIONAR'),
                    psg.Button('ALTERAR'),
                    psg.Button('REMOVER'),
                    psg.Button('VOLTAR')]]
-        self.__window = psg.Window('URNA ELETRÔNICA UFSC - CHAPAS', size=(1080,720)).Layout(layout)
+        layout = [[psg.Sizer(0, 300), psg.Column([[psg.Sizer(300, 0)]] + layout, element_justification='c', pad=(0, 0))]]
+        self.__window = psg.Window('URNA ELETRÔNICA UFSC - CHAPAS', layout, margins=(0,0))
 
     def tela_adiciona_chapa(self):
         psg.ChangeLookAndFeel('Reddit')
@@ -28,7 +29,8 @@ class TelaChapas():
                   [psg.Text('CODIGO'), psg.InputText(key='1')],
                   [psg.Text('NOME'), psg.InputText(key='2')],
                   [psg.Submit('SALVAR'), psg.Cancel('CANCELAR')]]
-        self.__window = psg.Window('URNA ELETRÔNICA UFSC - ADICIONAR CHAPA', size=(1080,720)).Layout(layout)
+        layout = [[psg.Sizer(0, 200), psg.Column([[psg.Sizer(500, 0)]] + layout, element_justification='c', pad=(0, 0))]]
+        self.__window = psg.Window('URNA ELETRÔNICA UFSC - ADICIONAR CHAPA', layout, margins=(0, 0))
 
     def tela_remove_chapa(self, chapa: list):
         psg.ChangeLookAndFeel('Reddit')
@@ -36,7 +38,8 @@ class TelaChapas():
                   [psg.Text(f'CODIGO: {chapa[0]}')],
                   [psg.Text(f'NOME: {chapa[1]}')],
                   [psg.Submit('CONFIRMAR'), psg.Cancel('CANCELAR')]]
-        self.__window = psg.Window('URNA ELETRÔNICA UFSC - REMOVER CHAPA', size=(1080,720)).Layout(layout)
+        layout = [[psg.Sizer(0, 200), psg.Column([[psg.Sizer(500, 0)]] + layout, element_justification='c', pad=(0, 0))]]
+        self.__window = psg.Window('URNA ELETRÔNICA UFSC - REMOVER CHAPA', layout, margins=(0, 0))
 
     def tela_altera_chapa(self, chapa:list):
         psg.ChangeLookAndFeel('Reddit')
@@ -44,7 +47,8 @@ class TelaChapas():
                   [psg.Text(f'CODIGO: {chapa[0]}')],
                   [psg.Text(f'NOME: '), psg.InputText(chapa[1], key='1')],
                   [psg.Submit('SALVAR'), psg.Cancel('CANCELAR')]]
-        self.__window = psg.Window('URNA ELETRÔNICA UFSC - ALTERAR CHAPA', size=(1080,720)).Layout(layout)
+        layout = [[psg.Sizer(0, 200), psg.Column([[psg.Sizer(500, 0)]] + layout, element_justification='c', pad=(0, 0))]]
+        self.__window = psg.Window('URNA ELETRÔNICA UFSC - ALTERAR CHAPA', layout, margins=(0, 0))
 
     def abre(self):
         event, values = self.__window.Read()
