5894f14230b9299461b4088849712e9f5ea505e3
Daniel <danielsottovia@gmail.com>
2022-12-12 16:46:56 -0300

    Revert "Tela Votação"
    This reverts commit 24d1bc2862c4733a9d9430f94db8451b98075df2.

--- limite/tela_votacao.py
+++ limite/tela_votacao.py
@@ -7,32 +7,28 @@ class TelaVotacao():
 
     def tela_opcoes_inicial(self, turno: int):
         psg.ChangeLookAndFeel('Reddit')
-        layout = [[psg.Button(f'INICIAR VOTAÇÃO DO {turno}º TURNO', key='INICIAR', size=(40,1))],
-                  [psg.Button('VOLTAR', key='VOLTAR', size=(40,1))]]
-        layout = [[psg.Sizer(0, 350), psg.Column([[psg.Sizer(250, 0)]] + layout, element_justification='c', pad=(0, 0))]]
-        self.__window = psg.Window('URNA ELETRÔNICA UFSC - VOTAÇÃO', layout, margins=(0, 0))
+        layout = [[psg.Button(f'INICIAR VOTAÇÃO DO {turno}º TURNO', key='INICIAR')],
+                  [psg.Button('VOLTAR', key='VOLTAR')]]
+        self.__window = psg.Window('URNA ELETRÔNICA UFSC - VOTAÇÃO', size=(1080,720)).Layout(layout)
 
     def tela_opcoes_votacao(self, turno: int):
         psg.ChangeLookAndFeel('Reddit')
-        layout = [[psg.Button(f'INICIAR NOVO VOTO', key='INICIAR', size=(40,1))],
-                  [psg.Button(f'ENCERRAR VOTAÇÃO DO {turno}º TURNO', key='ENCERRAR', size=(40,1))]]
-        layout = [[psg.Sizer(0, 350), psg.Column([[psg.Sizer(250, 0)]] + layout, element_justification='c', pad=(0, 0))]]
-        self.__window = psg.Window('URNA ELETRÔNICA UFSC - VOTAÇÃO', layout, margins=(0, 0))
+        layout = [[psg.Button(f'INICIAR NOVO VOTO', key='INICIAR')],
+                  [psg.Button(f'ENCERRAR VOTAÇÃO DO {turno}º TURNO', key='ENCERRAR')]]
+        self.__window = psg.Window('URNA ELETRÔNICA UFSC - VOTAÇÃO', size=(1080,720)).Layout(layout)
 
     def tela_selecao_eleitor(self, nome: str = '', categoria: str = ''):
         psg.ChangeLookAndFeel('Reddit')
         layout = [[psg.Text('DADOS DO ELEITOR')],
                   [psg.Text('CPF: '), psg.InputText(key='CPF')],
                   [psg.Submit('CONFIRMAR'), psg.Cancel('CANCELAR')]]
-        layout = [[psg.Sizer(0, 350), psg.Column([[psg.Sizer(250, 0)]] + layout, element_justification='c', pad=(0, 0))]]
-        self.__window = psg.Window('URNA ELETRÔNICA UFSC - VOTAÇÃO', layout, margins=(0, 0))
+        self.__window = psg.Window('URNA ELETRÔNICA UFSC - VOTAÇÃO', size=(1080,720)).Layout(layout)
 
     def tela_votacao_encerrada(self):
         psg.ChangeLookAndFeel('Reddit')
         layout = [[psg.Text('VOTAÇÕES ENCERRADAS!')],
                   [psg.Button('VOLTAR', key='VOLTAR')]]
-        layout = [[psg.Sizer(0, 350), psg.Column([[psg.Sizer(250, 0)]] + layout, element_justification='c', pad=(0, 0))]]
-        self.__window = psg.Window('URNA ELETRÔNICA UFSC - VOTAÇÃO', layout, margins=(0, 0))
+        self.__window = psg.Window('URNA ELETRÔNICA UFSC - VOTAÇÃO', size=(1080,720)).Layout(layout)
 
     def tela_seleciona_voto(self, cargo: str):
         psg.ChangeLookAndFeel('Reddit')
@@ -42,8 +38,7 @@ class TelaVotacao():
                   [psg.Button('4', key='4'),psg.Button('5', key='5'),psg.Button('6', key='6')],
                   [psg.Button('7', key='7'),psg.Button('8', key='8'),psg.Button('9', key='9')],
                   [psg.Button('0', key='0'),psg.Button('CORRIGIR'),psg.Button('CONFIRMAR')]]
-        layout = [[psg.Sizer(0, 350), psg.Column([[psg.Sizer(250, 0)]] + layout, pad=(0, 0))]]
-        self.__window = psg.Window('URNA ELETRÔNICA UFSC - VOTAÇÃO', layout, margins=(0, 0))
+        self.__window = psg.Window('URNA ELETRÔNICA UFSC - VOTAÇÃO', size=(1080,720)).Layout(layout)
 
     def tela_confirma_voto(self, cargo: str, num: int, chapa: str, nome: str):
         psg.ChangeLookAndFeel('Reddit')
@@ -52,8 +47,7 @@ class TelaVotacao():
                   [psg.Text(f'CHAPA: {chapa}')],
                   [psg.Text(f'NOME: {nome}')],
                   [psg.Submit('CONFIRMAR'),psg.Cancel('CANCELAR')]]
-        layout = [[psg.Sizer(0, 350), psg.Column([[psg.Sizer(250, 0)]] + layout, pad=(0, 0))]]
-        self.__window = psg.Window('URNA ELETRÔNICA UFSC - VOTAÇÃO', layout, margins=(0, 0))
+        self.__window = psg.Window('URNA ELETRÔNICA UFSC - VOTAÇÃO', size=(1080,720)).Layout(layout)
 
     def abre(self):
         event, values = self.__window.Read()
